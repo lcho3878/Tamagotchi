1b1673debebb307dd8abda7388f47ed7254ad368
lcho3878 <leech3878@naver.com>
2024-06-07 18:54:46 +0900

    User 모델 수정

--- Tamagotchi/Model/User.swift
+++ Tamagotchi/Model/User.swift
@@ -21,9 +21,29 @@ struct User {
             UserDefaults.standard.set(newValue, forKey: "name")
         }
     }
-    static var water = 0
-    static var rice = 0
+    static var water: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "water")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "water")
+        }
+    }
+    static var rice: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "rice")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "rice")
+        }
+    }
     static var level: Int {
-        return 9
+        let n: Double = Double((rice / 5) + (water / 2))
+        switch n {
+        case 0..<10: return 1
+        case 10...100: return Int(n / 10)
+        default:
+            return 10
+        }
     }
 }
