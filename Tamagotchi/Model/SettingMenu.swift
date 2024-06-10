8c308c3cc6ca513c3963a5debdc566f328797fb8
lcho3878 <leech3878@naver.com>
2024-06-10 09:12:00 +0900

    MenuType rawValue 추가

--- Tamagotchi/Model/SettingMenu.swift
+++ Tamagotchi/Model/SettingMenu.swift
@@ -9,21 +9,23 @@ import UIKit
 
 struct SettingMenu {
     
-    enum MenuType {
-        case changeName
-        case changeTamagotchi
-        case resetData
+    enum MenuType: String {
+        case changeName = "내 이름 설정하기"
+        case changeTamagotchi = "다마고치 변경하기"
+        case resetData = "데이터 초기화"
     }
     
     let image: UIImage
-    let title: String
     let type: MenuType
+    var title: String {
+        return type.rawValue
+    }
 }
 
 struct SettingMenuList {
     static let menuList = [
-        SettingMenu(image: UIImage(systemName: "pencil")!, title: "내 이름 설정하기", type: .changeName),
-        SettingMenu(image: UIImage(systemName: "moon.fill")!, title: "다마고치 변경하기", type: .changeTamagotchi),
-        SettingMenu(image: UIImage(systemName: "arrow.clockwise")!, title: "데이터 초기화", type: .resetData)
+        SettingMenu(image: UIImage(systemName: "pencil")!, type: .changeName),
+        SettingMenu(image: UIImage(systemName: "moon.fill")!, type: .changeTamagotchi),
+        SettingMenu(image: UIImage(systemName: "arrow.clockwise")!, type: .resetData)
     ]
 }
